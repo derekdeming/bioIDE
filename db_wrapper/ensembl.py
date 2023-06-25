f737c438e82df8df8369a9b25b1e5d865e799536
derekdeming <derekdeming17@gmail.com>
2023-06-25 08:15:54 -0600

    the framework for the wrappers

---
+++ db_wrapper/ensembl.py
@@ -0,0 +1,18 @@
+# ensembl.py
+from .base import BaseDatabase
+import requests
+
+class EnsemblDatabase(BaseDatabase):
+    def __init__(self):
+        super().__init__('ensembl', 'https://rest.ensembl.org')
+    
+    def get_endpoint_url(self, endpoint):
+        return f"{self.base_url}/{endpoint}"
+
+    def get_sequence_by_id(self, id):
+        response = self.get(f"sequence/id/{id}", headers={"Content-Type": "application/json"})
+        return response
+
+    def get_gene_by_id(self, id):
+        response = self.get(f"lookup/id/{id}", headers={"Content-Type": "application/json"})
+        return response

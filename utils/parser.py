e11f5f3a7a05546aea9ca861543baf6fdf03a19b
Derek <derekdeming17@gmail.com>
2023-06-30 09:48:45 -0600

    parser.py file modified  to have embedding node class

--- utils/parser.py
+++ utils/parser.py
@@ -1,8 +1,14 @@
 import concurrent.futures
 from typing import Dict, List
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from llama_index.node_parser import SimpleNodeParser
 from llama_index.data_structs import Node
 from llama_index.schema import Document
+from pathlib import Path
+from llama_index import download_loader
+from llama_index import Document
+import ray
+from ray.data import ActorPoolStrategy # to use Actors for parallelization -- map_batches() method
 
 # Step 1: Logic for parsing the files into llama_index documents - in our case it will be in formats of json (subject to change)
 def process_paper_data(paper_json):
@@ -56,4 +62,52 @@ def process_jsons_parallel(jsons: List[Dict]):
 
         return nodes
     
+# Embed each node using a local embedding model 
+class EmbedNodes:
+    def __init__(self):
+        '''
+        Use all-mpnet-base-v2 Sentence_transformer.
+        This is the default embedding model for LlamaIndex/Langchain.
+
+        Use GPU for embedding and specify a large enough batch size to maximize GPU utilization.
+        Remove the "device": "cuda" to use CPU instead.
+        '''
+        self.embedding_model = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-mpnet-base-v2", 
+            model_kwargs={"device": "cpu"},
+            encode_kwargs={"device": "cpu", "batch_size": 100}
+            )
+    
+    def __call__(self, node_batch: Dict[str, List[Node]]) -> Dict[str, List[Node]]:
+        nodes = node_batch["node"]
+        text = [node.text for node in nodes]
+        embeddings = self.embedding_model.embed_documents(text)
+        assert len(nodes) == len(embeddings)
+
+        for node, embedding in zip(nodes, embeddings):
+            node.embedding = embedding
+        return {"embedded_nodes": nodes}
+
+
+
+# HANDLING LOCAL FILES - NOT NEEDED FOR NOW
+# Step 0: Logic for loading and parsing the files into llama_index documents.
+# UnstructuredReader = download_loader("UnstructuredReader")
+# loader = UnstructuredReader()
+
+# def load_and_parse_files(file_row: Dict[str, Path]) -> Dict[str, Document]:
+#     documents = []
+#     file = file_row["path"]
+#     if file.is_dir():
+#         return []
+#     # Skip all non-html files like png, jpg, etc.
+#     if file.suffix.lower() == ".html":
+#         loaded_doc = loader.load_data(file=file, split_documents=False)
+#         loaded_doc[0].extra_info = {"path": str(file)}
+#         documents.extend(loaded_doc)
+#     return [{"doc": doc} for doc in documents]
+
+
+
+
 
